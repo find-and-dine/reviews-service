5829f0e3dcff42c2c862289366ccbc236711c823
kcelentano <katharinec@gmail.com>
2020-03-13 14:15:43 -0700

    Revise schema

--- database/schema.sql
+++ database/schema.sql
@@ -5,6 +5,35 @@ CREATE DATABASE guilt_tripadvisor;
 USE guilt_tripadvisor;
 
 /* Create other tables and define schemas for them here! */
+
+CREATE TABLE ads (
+  _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  imageURL VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE users (
+  _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  username VARCHAR(100) NOT NULL,
+  avatarURL VARCHAR(500),
+  reviewCount INT DEFAULT 0 NOT NULL,
+  postsCount INT DEFAULT 0 NOT NULL,
+  helpfulVotesCount INT DEFAULT 0 NOT NULL,
+  loc VARCHAR(50),
+  lev TINYINT
+);
+
+CREATE TABLE restaurants (
+  _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  restaurantName VARCHAR(100) NOT NULL,
+  claimed BOOLEAN
+);
+
+CREATE TABLE staff (
+  _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  staffName VARCHAR(100) NOT NULL,
+  position VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE reviews (
   _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   _restaurantID INT NOT NULL,
@@ -28,20 +57,16 @@ CREATE TABLE reviews (
   FOREIGN KEY(_staffResponseID) REFERENCES staff(_id)
 );
 
-CREATE TABLE ads (
+CREATE TABLE staffResponses (
   _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  imageURL VARCHAR(500) NOT NULL
-);
-
-CREATE TABLE users (
-  _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  username VARCHAR(100) NOT NULL,
-  avatarURL VARCHAR(500),
-  reviewCount INT DEFAULT 0 NOT NULL,
-  postsCount INT DEFAULT 0 NOT NULL,
-  helpfulVotesCount INT DEFAULT 0 NOT NULL,
-  loc VARCHAR(50),
-  lev TINYINT
+  _restaurantID INT NOT NULL,
+  _reviewID INT NOT NULL,
+  responseDate DATE NOT NULL,
+  body TEXT NOT NULL,
+  _staffID INT NOT NULL,
+  FOREIGN KEY(_restaurantID) REFERENCES restaurants(_id),
+  FOREIGN KEY(_reviewID) REFERENCES reviews(_id),
+  FOREIGN KEY(_staffID) REFERENCES staff(_id)
 );
 
 CREATE TABLE photos (
@@ -54,31 +79,7 @@ CREATE TABLE photos (
   imageURL VARCHAR(500),
   FOREIGN KEY(_restaurantID) REFERENCES restaurants(_id),
   FOREIGN KEY(_userID) REFERENCES users(_id),
-  FOREIGN KEY(_reviewID) REFERENCES review(_id)
-);
-
-CREATE TABLE staffResponses (
-  _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  _restaurantID INT NOT NULL,
-  _reviewID INT NOT NULL,
-  responseDate DATE NOT NULL,
-  body TEXT NOT NULL,
-  _staffID INT NOT NULL,
-  FOREIGN KEY(_restaurantID) REFERENCES restaurants(_id),
-  FOREIGN KEY(_reviewID) REFERENCES review(_id),
-  FOREIGN KEY(_staffID) REFERENCES staff(_id)
-);
-
-CREATE TABLE restaurants (
-  _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  restaurantName VARCHAR(100) NOT NULL,
-  claimed BOOLEAN
-);
-
-CREATE TABLE staffResponses (
-  _id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  staffName VARCHAR(100) NOT NULL,
-  position VARCHAR(100 NOT NULL)
+  FOREIGN KEY(_reviewID) REFERENCES reviews(_id)
 );
 
 /*  Execute this file from the command line by typing:
