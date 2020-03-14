c2df1b06a3b38d3868a8d867a7e7e8ebd1c31c5e
kcelentano <katharinec@gmail.com>
2020-03-13 21:19:24 -0700

    Refactor database connection with Sequalize

--- database/index.js
+++ database/index.js
@@ -1,16 +1,31 @@
-const mysql = require('mysql');
+const Sequelize = require('Sequelize');
+const db = new Sequelize('reviews', 'root', '');
+const User = require('./users.model.js');
+const Restaurant = require('./restaurants.model.js');
+var Review = require('./reviews.model.js');
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'guilt_tripadvisor',
-  multipleStatements: true
-});
+User.sync();
+Review.sync();
+Restaurant.sync();
 
-connection.connect((err) => {
-  if(!err) {
-    console.log('Connected to database');
-  } else {
-    console.log('Unable to connect to database. ', err);
-  }
-})
+exports.User = User;
+exports.Review = Review;
+exports.Restaurant = Restaurant;
+
+// Code below was prior to refactoring with Sequelize
+// const mysql = require('mysql');
+
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'guilt_tripadvisor',
+//   multipleStatements: true
+// });
+
+// connection.connect((err) => {
+//   if(!err) {
+//     console.log('Connected to database');
+//   } else {
+//     console.log('Unable to connect to database. ', err);
+//   }
+// })
