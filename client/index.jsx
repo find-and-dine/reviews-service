73aaf2024f4f0a64a39274fb809e7bcf4f693e2f
kcelentano <katharinec@gmail.com>
2020-03-25 14:28:53 -0700

    Change top div id tag and add aws deploy script

--- client/index.jsx
+++ client/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import path from 'path';
 import App from './components/App';
 
-ReactDOM.render(<App restaurantId={path.basename(document.URL)} />, document.getElementById('app'));
+ReactDOM.render(<App restaurantId={path.basename(document.URL)} />, document.getElementById('reviews_service'));
